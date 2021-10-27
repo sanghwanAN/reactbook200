ca99601b37f9c91847c843d9d9384fa9e0f22a46
sanghwanAN <vooo845@gmail.com>
2021-10-27 15:15:23 +0900

    리엑트200제 study 첫 커밋

---
+++ src/R004_LifecycleEx.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class R004_LifecycleEx extends Component {
+  render() {
+    console.log('3.render call');
+    return (
+      <h2>
+        asdfasdfasd
+      </h2>
+    )
+  }
+}
+
+export default R004_LifecycleEx;
---
+++ src/R005_LifecycleEx.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class R005_LifecycleEx extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    console.log('1 . constructor Call')
+  }
+  render() {
+    console.log('3. render Call');
+    return (
+      <h2>[ THIS IS RENDER FUCNTION ]</h2>
+    )
+  }
+}
+
+export default R005_LifecycleEx;
