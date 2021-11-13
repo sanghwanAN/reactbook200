53f66b8cbcb17e0ad47c0d3cf3ff7c7f6ff0be40
sanghwanAN <vooo845@gmail.com>
2021-11-13 15:19:05 +0900

    예제025 - finish

---
+++ src/R025_SetState.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class R025_SetState extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      StateString : "react",
+    }
+  }
+  StateChange = (flag) =>  {
+    if(flag == 'direct') this.state.StateString = "리액트";
+    if(flag == 'setState') this.setState({StateString : 'setState 리액트'});
+  }
+  render() {
+    return (
+      <div style={{padding: "0px"}}>
+        <button onClick={(e) => this.StateChange('direct', e)}>state 직접 변경</button>
+        <button onClick={(e) => this.StateChange('setState', e)}>state setState 변경</button> <br />
+        [state 변경하기] StateString : {this.state.StateString}
+      </div>
+    );
+  }
+}
+
+export default R025_SetState;
