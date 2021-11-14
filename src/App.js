46214574b59de4b014eb77d27ed37e004b65afb6
sanghwanAN <vooo845@gmail.com>
2021-11-14 16:32:13 +0900

    예제031 - finish

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import FunctionComponent from './R030_FunctionComponent'
+import ReactHook from './R031_ReactHook'
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <FunctionComponent contents="[This is FunctionComponent]"/>
+      <ReactHook/>
     </div>
   );
 }
---
+++ src/R031_ReactHook.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+
+function R031_ReactHook(props) {
+  const [contents, setContents] = useState('[This is React]');
+  useEffect( () => {
+    console.log('useEffect');
+  });
+  return (
+    <div style={{padding: "0px"}}>
+      <h2>{contents}</h2>
+      <button onClick={() =>setContents('[THIS IS HOOK]')}>버튼</button>
+    </div>
+  );
+}
+
+export default R031_ReactHook;
