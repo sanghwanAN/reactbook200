d8e26370b16ae9f92723e60cf4ae967b282a7d4f
sanghwanAN <vooo845@gmail.com>
2021-11-20 12:46:58 +0900

    예제060 - finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import './App.css';
-// import ReactstrapBadge from './R035_ReactstrapBadge'
-// import ReactstrapBreadcrumbs from './R036_ReactstrapBreadcrumbs'
-// import ReactstrapDropdown from './R037_ReactstrapDropdown'
-// import ReactstrapButtonGroup from './R038_ReactstrapButtonGroup'
-// import ReactstrapBottons from './R039_ReactstrapBottons'
-// import ReactstrapCard from './R040_ReactstrapCard'
-// import ReactstrapCarousel from './R041_ReactstrapCarousel'
-// import ReactstrapCollapse from './R042_ReactstrapCollapse'
-// import ReactstrapFade from './R043_ReactstrapFade'
-// import ReactstrapForm from './R044_ReactstrapForm'
-// import ReactstrapListGroup from './R047_ReactstrapListGroup'
-// import ReactstrapModal from './R048_ReactstrapModal'
-// import ReactstrapNavbar from './R049_ReactstrapNavbar'
-// import ReactstrapPopover from './R051_ReactstrapPopover'
-// import ReactstrapProgress from './R052_ReactstrapProgress'
-// import ReactstrapSpinner from './R053_ReactstrapSpinner'
-// import ReactstrapTable from './R054_ReactstrapTable'
-import ReactstrapTab from './R055_ReactstrapTab'
+import FetchGet from './R059_FetchGet'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
@@ -25,7 +8,7 @@ function App() {
     <div className="app">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapTab/>
+      <FetchGet/>
     </div>
   );
 }
