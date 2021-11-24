45fa5ca6b067a19b967a44db3acfabf5d3141581
sanghwanAN <vooo845@gmail.com>
2021-11-24 15:38:45 +0900

    예제084 - finish

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux'
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const CallMiddleware = store => nextMiddle => action => {
+    console.log('1. reducer 실행 전');
+    console.log('2. action.type : '+action.type+', store str : '+store.getState().data.str);
+    let result = nextMiddle(action);
+    console.log('3. reducer 실행 후');
+    console.log('4. action.type : '+action.type+', store str : '+store.getState().data.str);
+    return result;
+}
+
+const store = createStore(reducers, applyMiddleware(CallMiddleware));
 
 const listener = ()=> {
     ReactDOM.render(
