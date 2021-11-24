e27f9adcf15ce857f8c6c81e7666c9d1e57a1a61
sanghwanAN <vooo845@gmail.com>
2021-11-24 14:52:29 +0900

    예제078 ~ 080 - finish

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import reducers from './reducers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(reducers);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const listener = ()=> {
+    ReactDOM.render(
+        <Provider store={store}>
+            <App indexProp="react"/>
+        </Provider>,
+        document.getElementById('root')
+    );
+};
+
+store.subscribe(listener);
+listener();
