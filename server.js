738a0993912644cc631e84f8f2bb7a81c218a1e1
sanghwanAN <vooo845@gmail.com>
2021-11-30 12:35:39 +0900

    예제112 - finish

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ var app = express();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
